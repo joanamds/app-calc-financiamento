94e9fad5d203e0a531fe8888e344866f5c7dbd0a
joanamds <joanamdsantos1@gmail.com>
2024-10-26 09:16:46 -0300

    iniciando a organização para os cálculos

---
+++ src/calculations/interfaces/CalculationOption.ts
@@ -0,0 +1,7 @@
+enum CalculationOption {
+  PreFixedOnBalance = "A",             // Pré-fixado com juros sobre saldo devedor
+  PreFixedOnInstallment = "B",         // Pré-fixado com juros na parcela
+  PostFixedOnBalanceWithCorrection = "C",  // Pós-fixado com juros e correção no saldo devedor
+  PostFixedOnInstallmentWithCorrection = "D", // Pós-fixado com juros e correção na parcela
+  PostFixedOnBalanceWithInstallmentCorrection = "E" // Pós-fixado com juros sobre o saldo devedor e correção na parcela
+}
---
+++ src/calculations/interfaces/ILoanCalculation.ts
@@ -0,0 +1,12 @@
+interface ILoanCalculation {
+  loanAmount: number;             // Valor do empréstimo
+  interestRate: number;           // Taxa de juros
+  installmentCount: number;       // Número de parcelas
+  loanMonth: number;              // Mês de concessão do empréstimo
+  loanYear: number;               // Ano de concessão do empréstimo
+  correctionRate?: number;        // Taxa de correção monetária 
+  calculateInstallments(option: CalculationOption): number[]; // Opções de cálculo
+}
+
+
+export default ILoanCalculation;
