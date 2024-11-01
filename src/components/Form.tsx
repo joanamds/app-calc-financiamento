a77d5b0de8336817890d89d784f0edd9a4a4a496
Joana Maria dos Santos <joanamdsantos1@gmail.com>
2024-11-01 14:21:05 -0300

    consertando o getElementsBy

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -67,7 +67,7 @@ function LoanForm(): JSX.Element {
 
     const installmentDetails = calculator.calculateInstallments(loanAmount, Number(interestRate), installmentCount, loanMonth, loanYear, correctionRate);
     setTotalCalculation(installmentDetails);
-    const loanTableElement = document.getElementById("table-content"); 
+    const loanTableElement = document.getElementsByClassName("table-content"); 
     if (loanTableElement) {
       loanTableElement.scrollIntoView({ behavior: "smooth" });
     }
